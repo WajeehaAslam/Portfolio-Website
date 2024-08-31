6a37a940e284f062615cf419be7df983ca5f208b
Wajeeha Aslam <sp23-bai-054@cuilahore.edu.pk>
2024-08-31 12:23:06 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+
+
+
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
+
+
+
+
 window.onscroll = () => {
   sections.forEach(sec => {
     let top = window.scrollY;
